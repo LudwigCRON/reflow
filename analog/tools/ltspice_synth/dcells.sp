8bf7b4de9bc9c8d702c73c5c0bfdc521c655f162
Ludwig CRON <ludwig.cron@gmail.com>
2020-06-14 19:44:41 +0200

    Add ltspice digital cells support: - add liberty file for synthesis - add verilog file for pure digital simulation - add spice file for ltspice sim of synthesize digital

---
+++ analog/tools/ltspice_synth/dcells.sp
@@ -0,0 +1,150 @@
+***************************
+* BUFFERING
+***************************
+* differential buffer
+*            A Q QN
+*            | | |
+.SUBCKT BUFD 1 2 3
+A1 1 0 0 0 0 3 2 0 BUF
+.ENDS BUFD
+
+* single output buffer
+*            A Q
+*            | |
+.SUBCKT BUFS 1 2
+A1 1 0 0 0 0 0 2 0 BUF
+.ENDS BUFS
+
+* inverter
+*           A Q
+*           | |
+.SUBCKT NOT 1 2
+A1 1 0 0 0 0 2 0 0 BUF
+.ENDS NOT
+
+***************************
+* NANDS
+***************************
+* nand2
+*             A B Q
+*             | | |
+.SUBCKT NAND2 1 2 3
+A1 1 2 0 0 0 3 0 0 AND
+.ENDS NAND2
+
+* nand3
+*             A B C Q
+*             | | | |
+.SUBCKT NAND3 1 2 3 4
+A1 1 2 3 0 0 4 0 0 AND
+.ENDS NAND3
+
+* and2
+*            A B Q
+*            | | |
+.SUBCKT AND2 1 2 3
+A1 1 2 0 0 0 0 3 0 AND
+.ENDS AND2
+
+* and3
+*            A B C Q
+*            | | | |
+.SUBCKT AND3 1 2 3 4
+A1 1 2 3 0 0 0 4 0 AND
+.ENDS AND3
+
+***************************
+* NORS
+***************************
+* nor2
+*            A B Q
+*            | | |
+.SUBCKT NOR2 1 2 3
+A1 1 2 0 0 0 3 0 0 OR
+.ENDS NOR2
+
+* nor3
+*            A B C Q
+*            | | | |
+.SUBCKT NOR3 1 2 3 4
+A1 1 2 3 0 0 4 0 0 OR
+.ENDS NOR3
+
+* or2
+*           A B Q
+*           | | |
+.SUBCKT OR2 1 2 3
+A1 1 2 0 0 0 0 3 0 OR
+.ENDS OR2
+
+* or3
+*           A B C Q
+*           | | | |
+.SUBCKT OR3 1 2 3 4
+A1 1 2 3 0 0 0 4 0 OR
+.ENDS OR3
+
+***************************
+* XORS
+***************************
+* xor
+*            A B Q
+*            | | |
+.SUBCKT XOR2 A B Q
+A1 1 2 0 0 0 0 3 0 XOR
+.ENDS XOR2
+
+* xnor
+*             A B Q
+*             | | |
+.SUBCKT XNOR2 1 2 3
+A1 1 2 0 0 0 3 0 0 XOR
+.ENDS XNOR2
+
+***************************
+* LATCHES
+***************************
+* SR-latch
+*               S R Q QN
+*               | | | |
+.SUBCKT SRLATCH 1 2 3 4
+A1 1 2 0 0 0 4 3 0 SRFLOP Td=1ps
+.ENDS SRLATCH
+
+***************************
+* FLOPS
+***************************
+* DFF rising edge
+*           D C Q
+*           | | |
+.SUBCKT DFF 1 2 3
+A1 1 0 2 0 0 0 3 0 DFLOP Td=1ps
+.ENDS DFF
+
+* DFF rising edge with neg output
+*            D C Q QN
+*            | | | |
+.SUBCKT DFFQ 1 2 3 4
+A1 1 0 2 0 0 4 3 0 DFLOP Td=1ps
+.ENDS DFFQ
+
+* DFFRQ rising edge with reset and neg output
+*             D C R Q QN
+*             | | | | |
+.SUBCKT DFFRQ 1 2 3 4 5
+A1 1 0 2 4 0 5 4 0 DFLOP Td=1ps
+.ENDS DFFRQ
+
+* DFFSQ rising edge with set and neg output
+*             D C S Q QN
+*             | | | | |
+.SUBCKT DFFSQ 1 2 3 4 5
+A1 1 0 2 0 3 5 4 0 DFLOP Td=1ps
+.ENDS DFFSQ
+
+* DFFSRQ rising edge with set/reset and neg output
+*              D C R S Q QN
+*              | | | | | |
+.SUBCKT DFFSRQ 1 2 3 4 5 6
+A1 1 0 2 3 4 6 5 0 DFLOP Td=1ps
+.ENDS DFFSRQ
---
+++ analog/tools/ltspice_synth/dcells.v
@@ -0,0 +1,190 @@
+`timescale 1ns/100ps
+
+`ifndef LTSPICE_DCELLS
+`define LTSPICE_DCELLS
+
+module BUFD (
+    input  wire A,
+    output wire Q,
+    output wire QN
+);
+    buf g1(Q, A);
+    not g2(QN, A); 
+endmodule
+
+module BUFS (
+    input  wire A,
+    output wire Q
+);
+    buf g1(Q, A);
+endmodule
+
+module NOT (
+    input  wire A,
+    output wire Q
+);
+    not g1(Q, A);
+endmodule
+
+module NAND2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    nand g1(Q, A, B);
+endmodule
+
+module NAND3 (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    output wire Q
+);
+    nand g1(Q, A, B, C);
+endmodule
+
+module AND2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    and g1(Q, A, B);
+endmodule
+
+module AND3 (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    output wire Q
+);
+    and g1(Q, A, B, C);
+endmodule
+
+module NOR2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    nor g1(Q, A, B);
+endmodule
+
+module NOR3 (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    output wire Q
+);
+    nor g1(Q, A, B, C);
+endmodule
+
+module OR2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    or g1(Q, A, B);
+endmodule
+
+module OR3 (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    output wire Q
+);
+    or g1(Q, A, B, C);
+endmodule
+
+module XOR2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    xor g1(Q, A, B);
+endmodule
+
+module XNOR2 (
+    input  wire A,
+    input  wire B,
+    output wire Q
+);
+    xnor g1(Q, A, B);
+endmodule
+
+module SRLATCH (
+    input  wire S,
+    input  wire R,
+    output reg  Q,
+    output reg  QN
+);
+    always @(S or R)
+    begin
+        case ({S, R})
+            2'b01: begin Q = 1'b0; QN = 1'b1; end
+            2'b10: begin Q = 1'b1; QN = 1'b0; end
+            2'b11: begin Q = 1'bX; QN = 1'bX; end
+        endcase
+    end
+endmodule
+
+module DFF (
+    input  wire D,
+    input  wire C,
+    output reg  Q
+);
+    always @(posedge C)
+        Q <= D;
+endmodule
+
+module DFFQ (
+    input  wire D,
+    input  wire C,
+    output reg  Q,
+    output wire QN
+);
+    always @(posedge C)
+        Q <= D;
+    
+    not g1(QN, Q);
+endmodule
+
+module DFFRQ (
+    input  wire D,
+    input  wire C,
+    input  wire R,
+    output reg  Q,
+    output reg  QN
+);
+    always @(posedge C or posedge R)
+        Q <= (R) ? 1'b0 : D;
+    
+    not g1(QN, Q);
+endmodule
+
+module DFFSQ (
+    input  wire D,
+    input  wire C,
+    input  wire S,
+    output reg  Q,
+    output reg  QN
+);
+    always @(posedge C or posedge S)
+        Q <= (S) ? 1'b1 : D;
+    
+    not g1(QN, Q);
+endmodule
+
+module DFFSRQ (
+    input  wire D,
+    input  wire C,
+    input  wire S,
+    input  wire R,
+    output reg  Q,
+    output reg  QN
+);
+    always @(posedge C or posedge R or posedge S)
+        Q <= (S) ? 1'b1 : (R) ? 1'b0 : D;
+    
+    not g1(QN, Q);
+endmodule
+
+`endif
